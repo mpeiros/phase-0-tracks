1471f23d4d0346d1fd8fd86d1eb614413d3e8a29
Max Peiros <mpeiros@gmail.com>
2017-03-06 20:39:15 -0800

    Add for loop to process multiple employee surveys

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,43 @@
-puts 'What is your name'
-name = gets.chomp
+puts 'How many employees will be processed?'
+employees = gets.chomp.to_i
 
-puts 'How old are you?'
-age = gets.chomp.to_i
+for i in 1..employees
+  puts "Beginning survey for employee number #{i}"
 
-puts 'What year were you born?'
-year = gets.chomp.to_i
+  puts 'What is your name'
+  name = gets.chomp
 
-puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
-garlic_bread = gets.chomp
+  puts 'How old are you?'
+  age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+  puts 'What year were you born?'
+  year = gets.chomp.to_i
 
-result = 'Results inconclusive'
+  puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
+  garlic_bread = gets.chomp
 
-if (age == 2017 - year || age == 2017 - year - 1) && (garlic_bread == 'yes' || insurance == 'yes')
-  result = 'Probably not a vampire'
+  puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+
+  result = 'Results inconclusive'
+
+  if (age == 2017 - year || age == 2017 - year - 1) && (garlic_bread == 'yes' || insurance == 'yes')
+    result = 'Probably not a vampire'
+  end
+
+  if !(age == 2017 - year || age == 2017 - year - 1) && (garlic_bread == 'no' || insurance == 'no')
+    result = 'Probably a vampire'
+  end
+
+  if !(age == 2017 - year || age == 2017 - year - 1) && garlic_bread == 'no' && insurance == 'no'
+    result = 'Almost certainly a vampire'
+  end
+
+  if name == 'Drake Cula' || name == 'Tu Fang'
+    result = 'Definitely a vampire'
+  end
+
+  puts result
 end
 
-if !(age == 2017 - year || age == 2017 - year - 1) && (garlic_bread == 'no' || insurance == 'no')
-  result = 'Probably a vampire'
-end
-
-if !(age == 2017 - year || age == 2017 - year - 1) && garlic_bread == 'no' && insurance == 'no'
-  result = 'Almost certainly a vampire'
-end
-
-if name == 'Drake Cula' || name == 'Tu Fang'
-  result = 'Definitely a vampire'
-end
-
-print result
+puts 'All surveys completed!'
