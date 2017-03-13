0fbedffe5cd4f1ec67f550bf36ccfab280c86a7f
Max Peiros <mpeiros@gmail.com>
2017-03-12 22:49:11 -0700

    Add prompts for user input and create client hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,3 +6,35 @@
 #   Prompt user for new value and update the hash with the new value
 # Print out the new version of the hash for the user
 
+print "Enter the client's name: "
+name = gets.chomp
+
+print "Enter the client's age: "
+age = gets.chomp.to_i
+
+print "Enter how many kids the client has: "
+num_kids = gets.chomp.to_i
+
+print "Enter the client's preferred decor theme: "
+decor_theme = gets.chomp
+
+print "Enter the client's favorite color: "
+fav_color = gets.chomp
+
+print "Can the client afford an interior designer? "
+if gets.chomp.downcase == "yes"
+  afford = true
+else
+  afford = false
+end
+
+client = {
+  name: name,
+  age: age,
+  num_kids: num_kids,
+  decor_theme: decor_theme,
+  fav_color: fav_color,
+  afford: afford
+}
+
+puts client
