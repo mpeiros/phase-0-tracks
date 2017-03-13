32c47ca074e662cd6d2900278f8e4076db7ff323
Max Peiros <mpeiros@gmail.com>
2017-03-12 22:35:32 -0700

    Write pseudocode for the whole program

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+# Prompt user for name, age, address, number of kids, decor theme, favorite color, and if they can afford an interior designer
+# Assign user input to variables and perform any necessary conversions
+# Create a hash using the user input variables as values and print it to the user
+# Prompt the user if they would like to make any updates
+# If user wishes to make an update
+#   Prompt user for new value and update the hash with the new value
+# Print out the new version of the hash for the user
+
