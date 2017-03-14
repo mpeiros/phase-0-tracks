f1813629a21745c0c7bba42bd6424c656fe8a8d6
Max Peiros <mpeiros@gmail.com>
2017-03-13 19:11:52 -0700

    Create apartment_building nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,81 @@
+apartment_building = {
+  unit_one: {
+    pretty_name: 'Apartment 1',
+    tenants: [
+      'Bob',
+      'Joe'
+    ],
+    unit_info: {
+      bedrooms: 2,
+      baths: 1
+    },
+    pets: {
+      dogs: [
+        'Rover'
+      ],
+      cats: []
+    }
+  },
+  unit_two: {
+    pretty_name: 'Apartment 2',
+    tenants: [
+      'Gary',
+      'Fred',
+      'Beth'
+    ],
+    unit_info: {
+      bedrooms: 3,
+      baths: 2
+    },
+    pets: {
+      dogs: [],
+      cats: [
+        'Fluffy'
+      ]
+    }
+  },
+  unit_three: {
+    pretty_name: 'Apartment 3',
+    tenants: [
+      'Susan',
+      'Catherine'
+    ],
+    unit_info: {
+      bedrooms: 2,
+      baths: 2
+    },
+    pets: {
+      dogs: [
+        'Fido',
+        'Rocky'
+      ],
+      cats: [
+        'Missy'
+      ]
+    }
+  },
+  unit_four: {
+    pretty_name: 'Apartment 4',
+    tenants: [
+      'Jerry',
+      'George',
+      'Elaine',
+      'Kramer'
+    ],
+    unit_info: {
+      bedrooms: 4,
+      baths: 3
+    },
+    pets: {
+      dogs: [
+        'Beethoven'
+      ],
+      cats: [
+        'Hairball',
+        'Scratchy'
+      ]
+    }
+  }
+}
+
+puts apartment_building
