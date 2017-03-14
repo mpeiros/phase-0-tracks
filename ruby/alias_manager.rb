5507e6cddf7849928ee11814760f73faa8c72240
Max Peiros <mpeiros@gmail.com>
2017-03-13 22:52:43 -0700

    Refactor by adding separate method to replace chars

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -26,15 +26,13 @@ end
 
 def format_name(spy_name)
   name_array = spy_name.split
-  name_array.map! { |name| name.capitalize! }
+  name_array.map! { |name| name.capitalize }
   name_array.join(' ')
 end
 
-def create_alias(spy_name)
-  swapped_names = swap_names(spy_name).downcase
-  spy_alias = ''
-
-  swapped_names.each_char do |char|
+def replace_chars(spy_name)
+  spy_alias = ' '
+  spy_name.each_char do |char|
     if char == ' '
       spy_alias += ' '
     elsif char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'
@@ -43,7 +41,12 @@ def create_alias(spy_name)
       spy_alias += next_consonant(char)
     end
   end
+  spy_alias
+end
 
+def create_alias(spy_name)
+  swapped_names = swap_names(spy_name).downcase
+  spy_alias = replace_chars(swapped_names)
   format_name(spy_alias)
 end
 
