3b537d121163e7d4e322d32d4a70e90626ea356d
Max Peiros <mpeiros@gmail.com>
2017-03-18 19:39:57 -0700

    Add attribute-changing, getter, and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,43 +18,81 @@ class Santa
   def about
     puts "I am a #{@gender} #{@ethnicity} Santa!"
   end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    if @reindeer_ranking.include?(reindeer_name)
+      @reindeer_ranking.delete(reindeer_name)
+      @reindeer_ranking.push(reindeer_name)
+      p @reindeer_ranking
+    else
+      puts 'No reindeer by that name'
+    end
+  end
+
+  # Getter methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  # Setter methods
+  def gender=(new_gender)
+    @gender = new_gender
+  end
 end
 
+#Driver Code
+
+# Initializations for release 1
+# puts 'Making some santas'
+# puts '-' * 40
 # santas = []
-# santas << Santa.new("agender", "black")
-# santas << Santa.new("female", "Latino")
-# santas << Santa.new("bigender", "white")
-# santas << Santa.new("male", "Japanese")
-# santas << Santa.new("female", "prefer not to say")
-# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-# santas << Santa.new("N/A", "N/A")
+# example_santas = {
+#   'male' => 'black',
+#   'female' => 'white',
+#   'agender' => 'Chinese',
+#   'bigender' => 'Latino',
+#   'gender fluid' => 'unicorn'
+# }
 
-puts 'Making some santas'
-puts '-' * 40
-santas = []
-example_santas = {
-  'male' => 'black',
-  'female' => 'white',
-  'agender' => 'Chinese',
-  'bigender' => 'Latino',
-  'gender fluid' => 'unicorn'
-}
+# example_santas.each { |gender, ethnicity| santas << Santa.new(gender, ethnicity) }
+# santas.each { |santa| santa.about }
+# puts '-' * 40
 
-example_santas.each { |gender, ethnicity| santas << Santa.new(gender, ethnicity) }
-santas.each { |santa| santa.about }
-puts '-' * 40
+# puts 'Making some more santas'
+# puts '-' * 40
+# more_santas = []
+# more_example_santas = [
+#   ['female', 'Japanese'],
+#   ['male', 'German'],
+#   ['gender fluid', 'Russian'],
+#   ['bigender', 'Canadian'],
+#   ['agender', 'dragon']
+# ]
 
-puts 'Making some more santas'
-puts '-' * 40
-more_santas = []
-more_example_santas = [
-  ['female', 'Japanese'],
-  ['male', 'German'],
-  ['gender fluid', 'Russian'],
-  ['bigender', 'Canadian'],
-  ['agender', 'dragon']
-]
+# more_example_santas.each { |example_santa| more_santas << Santa.new(example_santa[0], example_santa[1]) }
+# more_santas.each { |santa| santa.about }
+# puts '-' * 40
 
-more_example_santas.each { |example_santa| more_santas << Santa.new(example_santa[0], example_santa[1]) }
-more_santas.each { |santa| santa.about }
-puts '-' * 40
+# Test code for relase 2
+santa_claus = Santa.new('male', 'black')
+santa_claus.about
+puts santa_claus.age
+puts santa_claus.ethnicity
+santa_claus.celebrate_birthday
+santa_claus.celebrate_birthday
+puts santa_claus.age
+santa_claus.get_mad_at('Vixen')
+santa_claus.get_mad_at('Vix')
+santa_claus.gender = 'female'
+santa_claus.about
+
+
+
