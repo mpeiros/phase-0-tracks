612cbacb4c480c5149fa6c40b0f2f379fcfcaa76
Max Peiros <mpeiros@gmail.com>
2017-03-19 15:03:13 -0700

    Create TodoList class and methods to pass tests

---
+++ ruby/list/todo-list.rb
@@ -0,0 +1,21 @@
+class TodoList
+  def initialize(items)
+    @items = items
+  end
+
+  def get_items
+    @items
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def delete_item(item)
+    @items.delete(item)
+  end
+
+  def get_item(index)
+    @items[index]
+  end
+end
