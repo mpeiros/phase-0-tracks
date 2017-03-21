4ad654aec9aa5f7439a3d783d0e7733ab0efebb8
Max Peiros <mpeiros@gmail.com>
2017-03-20 19:29:23 -0700

    Add tests and refactor to use ternary operator

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,39 +1,84 @@
+# Method to print a list and make it look pretty
+def print_list(list)
+  puts "Your grocery list:"
+  # input: list
+  # steps:
+  # iterate through the list and print out items and quantities
+  list.each { |item, quantity| puts "#{item}: #{quantity}" }
+  # item: quantity
+  # output: printed list in a nice format
+end
+
 # Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
+# input: string of items separated by spaces (example: “carrots apples cereal pizza”)
+def create_list(str)
+  grocery_list = {}
+  # steps:
   # split input into array
+  str_array = str.split
   # iterate array and add each item to hash
+  str_array.each { |item| grocery_list[item] = 1 }
   # set default quantity
   # print the list using print method and passing in hash
-# output: return the created hash
+  # print_list(grocery_list)
+  # output: return the created hash
+  grocery_list
+end
+
+# puts create_list("apples pears cherries")
 
 # Method to add an item to a list
-# input: list, item name, and optional quantity
-# steps:
+def add_item(list, item_name, quantity=1)
+  # input: list, item name, and optional quantity
+  # steps:
   # check IF item is included in list
-    # increment item by quantity
-    # ELSE - set item as new key and assign it to quantity as value
-# output: return the updated hash
+  list.include?(item_name) ? list[item_name] += quantity : list[item_name] = quantity
+
+  # output: return the updated hash
+  # puts "Here is your updated list:"
+  list
+end
+
+# test_list = create_list("apples pears cherries")
+# add_item(test_list, "peaches", 3)
+# test_list
 
 # Method to remove an item from the list
-# input: list, item name
-# steps:
+def remove_item(list, item_name)
+  # input: list, item name
+  # steps:
   # check IF item is included in list
-    # delete the item
-    # ELSE, print a message to the user
-# output: return updated hash or the original hash
+  list.include?(item_name) ? list.delete(item_name) : puts("No #{item_name} on the list.")
+  
+  # output: return updated hash or the original hash
+  list
+end
+
+# puts remove_item(test_list, "cars")
 
 # Method to update the quantity of an item
-# input: list, item name, new quantity
-# steps:
+def update_quantity(list, item_name, quantity)
+  # input: list, item name, new quantity
+  # steps:
   # check IF item is included in list
-    # update the item's quantity
-    # ELSE, print a message to the user
-# output: return the updated hash or the original hash
+  list.include?(item_name) ? list[item_name] = quantity : puts("No #{item_name} on the list.")
 
-# Method to print a list and make it look pretty
-# input: list
-# steps: 
-  # iterate through the list and print out items and quantities
-    # item: quantity
-# output: printed list in a nice format
+  # output: return the updated hash or the original hash
+  list
+end
+
+# puts update_quantity(test_list, "cars", 4)
+
+# Test code for release 2
+grocery_list = create_list("")
+add_item(grocery_list, "lemonade", 2)
+add_item(grocery_list, "tomatoes", 3)
+add_item(grocery_list, "onions", 1)
+add_item(grocery_list, "ice cream", 4)
+print_list(grocery_list)
+puts '-' * 40
+remove_item(grocery_list, "lemonade")
+print_list(grocery_list)
+puts '-' * 40
+update_quantity(grocery_list, "ice cream", 1)
+print_list(grocery_list)
