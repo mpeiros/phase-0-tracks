6de671388f4fed9bfb4727690aa41c93cdbdbae1
Max Peiros <mpeiros@gmail.com>
2017-03-20 20:09:10 -0700

    Add answers for Release 4: Reflect to end of file

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,7 +1,7 @@
 # Method to print a list and make it look pretty
+# input: list
 def print_list(list)
   puts "Your grocery list:"
-  # input: list
   # steps:
   # iterate through the list and print out items and quantities
   list.each { |item, quantity| puts "#{item}: #{quantity}" }
@@ -64,7 +64,7 @@ def update_quantity(list, item_name, quantity)
 end
 
 # Test code for release 2
-grocery_list = create_list("")
+grocery_list = create_list("apples oranges milk")
 add_item(grocery_list, "lemonade", 2)
 add_item(grocery_list, "tomatoes", 3)
 add_item(grocery_list, "onions", 1)
@@ -77,4 +77,10 @@ puts '-' * 40
 update_quantity(grocery_list, "ice cream", 1)
 print_list(grocery_list)
 
-
+# Release 4: Reflect
+# 1. I learned that pseudocode is a nice way to lay out the design of your entire program before writing the actual code. It can also make the code itself easier to write.
+# 2. A hash makes more sense for this challenge as an item name and its quantity is a logical key-value pair. Also, it makes sense that you would want to access the item by its name. If you had used an array, it would have required nesting [item, quantity] arrays inside the list array or tracking the index of each item and its associated quantity, both of which would be more complex than just using a hash.
+# 3. A method returns its last evaluated expression. A method can return early through use of the explicit return keyword.
+# 4. You can pass anything you want - integers, strings, hashes, arrays, etc.
+# 5. You can pass information between methods by passing in the right arguments (and having the methods require the right parameters as well). For example, the information in the grocery_list hash created above is passed between the different methods by passing the grocery_list hash to each method as an argument.
+# 6. I think the concept of pass by reference was solidified with this challenge. I also became more comfortable with the ternary operator and learned the importance of method return values to testing. I think I have a solid grasp of the concepts in this challenge and do not have any confusion at the moment.
