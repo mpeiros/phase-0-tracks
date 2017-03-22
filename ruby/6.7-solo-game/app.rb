0db047d88d298b14f7c22bfeea34125f9cca33e3
Max Peiros <mpeiros@gmail.com>
2017-03-22 12:30:58 -0700

    Update app.rb to make user interface a bit more readable

--- ruby/6.7-solo-game/app.rb
+++ ruby/6.7-solo-game/app.rb
@@ -9,17 +9,18 @@ user_word = gets.chomp.downcase
 game = WordGame.new(user_word)
 
 until game.game_is_over?
+  puts '_' * 50
   puts "The status of the word you must guess is listed below. You have #{game.max_wrong_guesses} wrong guesses remaining."
   puts game.guessed_word
 
   print 'Please enter a letter to guess: '
   user_guess = gets.chomp.downcase
 
-  puts 'You have already guessed that letter.' unless game.guess_letter(user_guess)
-  game.check_for_win
+  puts "You have already guessed the letter #{user_guess}." unless game.guess_letter(user_guess)
+  user_win = game.check_for_win
 end
 
-if game.check_for_win
+if user_win
   puts("Congrats! You guessed the word #{game.word} with #{game.max_wrong_guesses} wrong guesses remaining.") 
 else
   puts("You suck! You were unable to guess the word #{game.word}.")
