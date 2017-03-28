18a6d4c8fbc9cc1ce6d4a4ee0678763fb7bde25f
Max Peiros <mpeiros@gmail.com>
2017-03-27 21:41:23 -0700

    Create explore.js and add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Function takes a string and returns the reversed string
+// create an empty string that will accumulate the reversed characters
+// loop through the string passed in as the argument in reverse
+// inside the loop, grab the current character and add it to the accumulator
+// return the accumulator string
+
