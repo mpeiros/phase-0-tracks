b0064cc5b59fbb6301fba8aeabed31503d2b38e8
Max Peiros <mpeiros@gmail.com>
2017-03-27 21:53:41 -0700

    Add function to reverse string

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,19 @@
 // inside the loop, grab the current character and add it to the accumulator
 // return the accumulator string
 
+function reverseString(str) {
+  revStr = '';
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    letter = str[i];
+    revStr += letter;
+  }
+
+  return revStr;
+}
+
+var reversedString = reverseString('Javascript');
+
+if (!false) {
+  console.log(`Javascript reversed is ${reversedString}`);
+}
