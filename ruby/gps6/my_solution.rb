a1c7249dcd1defca627dfe71ad65e67a1a86b77a
Max Peiros <mpeiros@gmail.com>
2017-03-29 20:14:07 -0700

    Refactor predicted_deaths and speed_of_spread instance methods

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -28,20 +28,19 @@ class VirusPredictor
   # calculates amount of predicted deaths based on population density and population, prints the result
   def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
+    population_multiplier = 0
+
+    case @population_density
+    when (200..Float::INFINITY) then population_multiplier = 0.4
+    when (150...200) then population_multiplier = 0.3
+    when (100...150) then population_multiplier = 0.2
+    when (50...100) then population_multiplier = 0.1
+    else population_multiplier = 0.05
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    number_of_deaths = (@population * population_multiplier).floor
 
+    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
   end
 
   # calculates how fast the flu is spreading based on population density, prints the result
@@ -50,20 +49,15 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    case @population_density
+    when (200..Float::INFINITY) then speed = 0.5
+    when (150...200) then speed = 1
+    when (100...150) then speed = 1.5
+    when (50...100) then speed = 2
+    else speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
