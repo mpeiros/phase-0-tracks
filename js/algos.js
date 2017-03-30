15e253d0ddd51d93d4abc5c86a019e6648288dca
Max Peiros <mpeiros@gmail.com>
2017-03-30 09:38:00 -0700

    Add pseudocode for function to find the longest phrase

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Release 0: Find the Longest Phrase
+// function that takes array of strings and returns the longest one
+// assign first string in the array to a longestWord variable
+// starting at the second string in the array, loop through the array and compare lengths to longestWord.length
+// if length of comparison string is greater, reassign that string to longestWord
+// after looping through the array, return longestWord
+
+function findLongestWord(words) {
+  
+}
