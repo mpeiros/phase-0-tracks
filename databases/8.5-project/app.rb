95402819eead4918ba0757f38298380008cf9db2
Max Peiros <mpeiros@gmail.com>
2017-04-06 18:55:17 -0700

    Add httparty gem and get test request and json parsing to work

--- databases/8.5-project/app.rb
+++ databases/8.5-project/app.rb
@@ -1,4 +1,6 @@
 require 'sqlite3'
+require 'httparty'
+require 'json'
 
 db = SQLite3::Database.new('test.db')
 
@@ -7,7 +9,16 @@ create_table_cmd = <<-SQL
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     age INT
-  )
+  );
 SQL
 
 db.execute(create_table_cmd)
+
+response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow', format: :plain)
+
+# puts response.body, response.code, response.message, response.headers.inspect
+
+puts response.code, response.message 
+
+my_hash = JSON.parse(response, symbolize_names: true)
+puts my_hash[:items][0][:tags]
