bc326df6d5a178cffa79d0385e1eeecd03725254
Max Peiros <mpeiros@gmail.com>
2017-04-07 15:25:29 -0700

    Fill in driver code skeleton in app.rb

--- databases/8.5-project/app.rb
+++ databases/8.5-project/app.rb
@@ -4,7 +4,7 @@ require_relative 'weather_data'
 # Helper print method
 def options_menu
   puts "\nSelect one of the following options:"
-  puts 'Enter 1 to add a city to your list and get its weather information'
+  puts 'Enter 1 to add a city to your list'
   puts 'Enter 2 to get weather information for a city on your list'
   puts 'Enter 3 to update a city on your list'
   puts 'Enter 4 to delete a city from your list' 
@@ -27,40 +27,28 @@ loop do
 
   case user_choice.to_i
   when 1
-    puts 'Selected 1'
+    print 'Enter the city you wish to add: '
+    city = gets.chomp.strip
+    ds.add_city(city)
   when 2
-    puts 'Selected 2'
+    print 'Enter the number of the city you wish to get weather information for: '
+    id = gets.chomp.to_i
+    city = ds.get_city(id)
+    weather_data = WeatherData.new(city)
+    weather_data.get_weather ? weather_data.print_weather_info : puts('Error: Not a valid city')
   when 3
-    puts 'Selected 3'
+    print 'Enter the number of the city you wish to update: '
+    id = gets.chomp.to_i
+    print 'Enter the updated name for this city: '
+    new_name = gets.chomp
+    ds.update_city(id, new_name)
   when 4
-    puts 'Selected 4'
+    print 'Enter the number of the city you wish to delete: '
+    id = gets.chomp.to_i
+    ds.delete_city(id)
   else
     puts 'Please enter a valid option!'
   end
 end
 
 puts "\nSee you next time!"
-
-# print "\nEnter id:"
-# id = gets.chomp.to_i
-# print 'Enter city name: '
-# city_name = gets.chomp
-# ds.update_city(id, city_name)
-
-# print 'Enter a city: '
-# id = gets.chomp.to_i
-# ds.delete_city(id)
-
-# id = gets.chomp.to_i
-# city = ds.get_city(id)
-
-# city = gets.chomp.strip
-# ds.add_city(city)
-
-# weather_data = WeatherData.new(city)
-
-# if weather_data.get_weather
-#   weather_data.print_weather_info
-# else
-#   puts 'error - not a valid city'
-# end
