095b69b0ff438de3416d4a957ad53cca1cc8d4d4
Max Peiros <mpeiros@gmail.com>
2017-04-07 15:12:44 -0700

    Create skeleton of driver code in app.rb

--- databases/8.5-project/app.rb
+++ databases/8.5-project/app.rb
@@ -1,17 +1,51 @@
 require_relative 'data_service'
 require_relative 'weather_data'
 
+# Helper print method
+def options_menu
+  puts "\nSelect one of the following options:"
+  puts 'Enter 1 to add a city to your list and get its weather information'
+  puts 'Enter 2 to get weather information for a city on your list'
+  puts 'Enter 3 to update a city on your list'
+  puts 'Enter 4 to delete a city from your list' 
+  puts "Enter 'q' to quit"
+end
+
+# Instantiate data service to handle SQL database
 ds = DataService.new
 
-puts 'Here is your list of favorite cities:'
-all = ds.select_all
-ds.print_results(all)
+# Driver code
+loop do
+  puts "\nYour list of favorite cities:"
+  all_cities = ds.select_all
+  ds.print_results(all_cities)
+  options_menu
 
-print 'Enter id: '
-id = gets.chomp.to_i
-print 'Enter city name: '
-city_name = gets.chomp
-ds.update_city(id, city_name)
+  print "\nEnter your choice: "
+  user_choice = gets.chomp
+  break if user_choice == 'q'
+
+  case user_choice.to_i
+  when 1
+    puts 'Selected 1'
+  when 2
+    puts 'Selected 2'
+  when 3
+    puts 'Selected 3'
+  when 4
+    puts 'Selected 4'
+  else
+    puts 'Please enter a valid option!'
+  end
+end
+
+puts "\nSee you next time!"
+
+# print "\nEnter id:"
+# id = gets.chomp.to_i
+# print 'Enter city name: '
+# city_name = gets.chomp
+# ds.update_city(id, city_name)
 
 # print 'Enter a city: '
 # id = gets.chomp.to_i
